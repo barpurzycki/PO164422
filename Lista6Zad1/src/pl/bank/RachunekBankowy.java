205d4940fab87c453edcae36977e0169f0945a8d
barpurzycki <bartekpurzycki01@gmail.com>
2023-01-02 15:09:56 +0100

    zad
    zad

---
+++ Lista6Zad1/src/pl/bank/RachunekBankowy.java
@@ -0,0 +1,27 @@
+package pl.bank;
+
+public class RachunekBankowy {
+
+
+    public RachunekBankowy(double saldo)
+    {
+        this.saldo = saldo;
+    }
+
+    public void obliczMiesieczneOdsetki()
+    {
+        this.saldo += (this.saldo * rocznaStopaProcentowa) / 12;
+    }
+
+    public static void setRocznaStopaProcentowa(double nowa)
+    {
+        rocznaStopaProcentowa = nowa;
+    }
+
+    public double getSaldo()
+    {
+        return this.saldo;
+    }
+    private double saldo;
+    private static double rocznaStopaProcentowa;
+}
